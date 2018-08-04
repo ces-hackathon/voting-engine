d8fc42985a04075f0fe70226ea283f4e9fd3ba61
Samuel Eaton <samuel.eaton@capco.com>
2018-08-04 08:41:04 -0500

    Updated Game schema - added votes, location, start_time

--- api/models/gameModel.js
+++ api/models/gameModel.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var mongoose = require('mongoose');
 var moment = require('moment');
 var Schema = mongoose.Schema;
@@ -9,11 +10,24 @@ var GameSchema = new Schema({
     type: String,
     required: 'Kindly enter the name of the lunch train'
   },
-  restaurants: {type: [String]},
-  users: {type: [String]},
+  restaurants: {
+      type: [String]
+  },
+  votes: [{ restaurant: String, user: String, voteUp: Boolean }],
+  users: {
+      type: [String],
+      required: 'At least one user is required to begin a lunch train'
+  },
+  location: {
+    type: String
+  },
   created_date: {
     type: Date,
     default: moment().toDate()
+  },
+  start_time: {
+    type: Date,
+    required: 'Desired lunch time must be entered'
   },
   expiration_date: {
     type: Date,
