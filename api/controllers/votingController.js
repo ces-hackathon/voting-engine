d23dffb280075924b1e9748220eabdbb63076fc3
Samuel Eaton <samuel.eaton@capco.com>
2018-08-04 09:48:37 -0500

    updated variable naming

--- api/controllers/votingController.js
+++ api/controllers/votingController.js
@@ -1,56 +1,64 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-Game = mongoose.model('Games');
+Games = mongoose.model('Games');
 
 exports.list_all_games = function(req, res) {
-  Game.find({}, function(err, task) {
+  Games.find({}, function(err, trains) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(trains);
   });
 };
 
 exports.create_a_game = function(req, res) {
-  var new_game = new Game(req.body);
-  new_game.save(function(err, task) {
+  var new_game = new Games(req.body);
+  new_game.save(function(err, trains) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(trains);
   });
 };
 
 exports.get_game_votes = function(req, res) {
-  Game.findById(req.params.taskId, function(err, task) {
+  Games.findById(req.params.gameId, function(err, trains) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(trains);
   });
 };
 
 exports.cast_a_vote = function(req, res) {
-    Game.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    Games.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, trains) {
       if (err)
         res.send(err);
-      res.json(task);
+      res.json(trains);
     });
   };
 
 exports.update_a_game = function(req, res) {
-  Game.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+  Games.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, trains) {
     if (err)
       res.send(err);
-    res.json(task);
+    res.json(trains);
   });
 };
 
 
 exports.delete_a_game = function(req, res) {
-  Game.remove({
+  Games.remove({
     _id: req.params.taskId
-  }, function(err, task) {
+  }, function(err, trains) {
     if (err)
       res.send(err);
-    res.json({ message: 'Game successfully deleted' });
+    res.json({ message: 'Games successfully deleted' });
   });
 };
+
+exports.add_user_to_game = function(req, res) {
+    Games.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, trains) {
+      if (err)
+        res.send(err);
+      res.json(trains);
+    });
+  };
