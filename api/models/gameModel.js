74652b71735d1ce8e04875e96c1a42773b6fd341
Samuel Eaton <samuel.eaton@capco.com>
2018-08-03 23:23:13 -0500

    add moment.js and gameModel.js

---
+++ api/models/gameModel.js
@@ -0,0 +1,29 @@
+'use strict';
+var mongoose = require('mongoose');
+var moment = require('moment');
+var Schema = mongoose.Schema;
+
+
+var GameSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  Created_date: {
+    type: Date,
+    default: moment()
+  },
+  Expiration_date: {
+    type: Date,
+    default: moment().add(5, 'minutes')
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+
+module.exports = mongoose.model('Games', GameSchema);
