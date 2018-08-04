ca4c8efe9a48129fc2aaeec2154eb6f8b6aa8b0c
Samuel Eaton <samuel.eaton@capco.com>
2018-08-03 22:50:25 -0500

    added mongoose

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var msg = 'Hello World';
-console.log(msg);
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
