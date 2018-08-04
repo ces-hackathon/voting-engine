f227b053e9510308655ccb9a1a133c9511c94e48
Samuel Eaton <samuel.eaton@capco.com>
2018-08-03 23:31:19 -0500

    Initial creation of voting routes

---
+++ api/routes/votingRoutes.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(app) {
+  var voter = require('../controllers/votingController');
+
+  // Voting Routes
+  app.route('/games')
+    .get(voter.list_all_games)
+    .post(voter.create_a_game);
+
+
+  app.route('/games/:gameId')
+    .get(voter.get_game_votes)
+    .post(voter.cast_a_vote)
+    .put(voter.update_a_game)
+    .delete(voter.delete_a_game);
+};
