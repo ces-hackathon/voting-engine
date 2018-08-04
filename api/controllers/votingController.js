d60f5d1252e1b001d2d20f1285606c851daf9a80
Samuel Eaton <samuel.eaton@capco.com>
2018-08-03 23:39:07 -0500

    initial voting controller

---
+++ api/controllers/votingController.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+Game = mongoose.model('Games');
+
+exports.list_all_games = function(req, res) {
+  Game.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.create_a_game = function(req, res) {
+  var new_game = new Game(req.body);
+  new_game.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.get_game_votes = function(req, res) {
+  Game.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.cast_a_vote = function(req, res) {
+    Game.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+      if (err)
+        res.send(err);
+      res.json(task);
+    });
+  };
+
+exports.update_a_game = function(req, res) {
+  Game.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.delete_a_game = function(req, res) {
+  Game.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Game successfully deleted' });
+  });
+};
